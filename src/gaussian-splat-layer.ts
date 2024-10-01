cef59de5d89976a53b7c3f4382fbc5101f5e71f2
tebben <ebben.tim@gmail.com>
2024-10-01 10:21:04 +0200

    added extra placement keystrokes: https://github.com/tebben/cesium-gaussian-splatting/issues/1

--- src/gaussian-splat-layer.ts
+++ src/gaussian-splat-layer.ts
@@ -6,50 +6,112 @@ export class GaussianSplatLayer {
   public scene!: THREE.Scene;
   public splatViewer: GaussianSplats3D.Viewer;
   public ready: boolean;
-
   private model: string;
   private location: { lon: number; lat: number; height: number };
   private rotation: { x: number; y: number; z: number };
+  private scale: number;
 
   constructor(
     model: string,
     location: { lon: number; lat: number; height: number },
-    rotation: { x: number; y: number; z: number }
+    rotation: { x: number; y: number; z: number },
+    initialScale: number = 1
   ) {
     this.ready = false;
     this.model = model;
     this.location = location;
     this.rotation = rotation;
+    this.scale = initialScale;
 
-    // A b*tch to get the rotation correctly
-    // so adding some keys to debug and find the rotation
     window.addEventListener("keydown", (event) => {
-      this.rotateScene(event);
+      this.adjustScene(event);
     });
   }
 
-  private rotateScene(event: KeyboardEvent) {
-    if (event.key === "q") {
-      this.scene.rotateY(0.05);
-    } else if (event.key === "w") {
-      this.scene.rotateY(-0.05);
-    } else if (event.key === "a") {
-      this.scene.rotateX(0.05);
-    } else if (event.key === "s") {
-      this.scene.rotateX(-0.05);
-    } else if (event.key === "z") {
-      this.scene.rotateZ(0.05);
-    } else if (event.key === "x") {
-      this.scene.rotateZ(-0.05);
+  private adjustScene(event: KeyboardEvent) {
+    switch (event.key) {
+      case "q":
+        this.scene.rotateY(0.005);
+        break;
+      case "w":
+        this.scene.rotateY(-0.005);
+        break;
+      case "a":
+        this.scene.rotateX(0.005);
+        break;
+      case "s":
+        this.scene.rotateX(-0.005);
+        break;
+      case "z":
+        this.scene.rotateZ(0.005);
+        break;
+      case "x":
+        this.scene.rotateZ(-0.005);
+        break;
+      case "y":
+        this.location.lat += 0.0000025;
+        this.updatePosition();
+        break;
+      case "h":
+        this.location.lat -= 0.0000025;
+        this.updatePosition();
+        break;
+      case "j":
+        this.location.lon += 0.0000025;
+        this.updatePosition();
+        break;
+      case "g":
+        this.location.lon -= 0.0000025;
+        this.updatePosition();
+        break;
+      case "o": // Height increment
+        this.location.height += 1;
+        this.updatePosition();
+        break;
+      case "l": // Height decrement
+        this.location.height -= 1;
+        this.updatePosition();
+        break;
+      case "m":
+        this.scale += 0.05;
+        this.updateScale();
+        break;
+      case "n":
+        if (this.scale > 0.05) {
+          this.scale -= 0.05;
+          this.updateScale();
+        }
+        break;
     }
 
     console.log(
-      "roation",
+      "Model:",
       this.model,
+      "\nRotation:",
       this.scene.rotation.x,
       this.scene.rotation.y,
-      this.scene.rotation.z
+      this.scene.rotation.z,
+      "\nLoc:",
+      `Lon: ${this.location.lon}, Lat: ${this.location.lat}, Height: ${this.location.height}`,
+      "\nScale:",
+      this.scale
     );
+  }
+
+  private updatePosition() {
+    const position = Cesium.Cartesian3.fromDegrees(
+      this.location.lon,
+      this.location.lat,
+      this.location.height
+    );
+    this.scene.position.set(position.x, position.y, position.z);
+  }
+
+  private updateScale() {
+    if (this.splatViewer.getSplatMesh()) {
+      const mesh = this.splatViewer.getSplatMesh();
+      mesh.scale.set(this.scale, this.scale, this.scale);
+    }
   }
 
   public setup(camera: THREE.Camera, renderer: THREE.Renderer) {
@@ -77,6 +139,7 @@ export class GaussianSplatLayer {
     this.scene = new THREE.Scene();
     this.scene.position.set(position.x, position.y, position.z);
     this.scene.rotation.set(this.rotation.x, this.rotation.y, this.rotation.z);
+
     this.splatViewer
       .addSplatScene(this.model, {
         showLoadingUI: false,
@@ -85,7 +148,9 @@ export class GaussianSplatLayer {
         scale: [1, 1, 1],
       })
       .then(() => {
-        this.scene.add(this.splatViewer.getSplatMesh());
+        const mesh = this.splatViewer.getSplatMesh();
+        mesh.scale.set(this.scale, this.scale, this.scale);
+        this.scene.add(mesh);
         this.ready = true;
       });
   }
